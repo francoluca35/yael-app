51184bd7193bf9fba028c3913977448e6b3c1d7e
franco luca parera <francolucap1@gmail.com>
2025-06-27 22:19:55 -0300

    .

--- src/app/reserva-sucess/page.js
+++ src/app/reserva-sucess/page.js
@@ -1,40 +1,12 @@
 "use client";
-import { Suspense, useEffect } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
-import { v4 as uuidv4 } from "uuid";
+import { Suspense } from "react";
+import ReservaSuccessContent from "./components/ReservaSuccessContent";
 
-export default function ReservaSuccess() {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-
-  useEffect(() => {
-    const success = searchParams.get("success");
-    const data = JSON.parse(localStorage.getItem("formReservaEvento") || "{}");
-
-    if (success === "true" && data) {
-      fetch("/api/reserved", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          ...data,
-          id: uuidv4(),
-        }),
-      }).then((res) => {
-        if (res.ok) {
-          alert("¡Reserva confirmada!");
-          localStorage.removeItem("formReservaEvento");
-          router.replace("/");
-        } else {
-          alert("Error al guardar reserva");
-        }
-      });
-    }
-  }, [searchParams]);
-
+export default function ReservaSuccessPage() {
   return (
     <div className="text-white p-8">
       <Suspense fallback={<div className="text-white p-4">Cargando...</div>}>
-        Procesando reserva...
+        <ReservaSuccessContent />
       </Suspense>
     </div>
   );
